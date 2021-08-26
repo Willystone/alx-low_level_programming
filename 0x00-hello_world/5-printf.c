aec81379a7d4e2697be72a191ef7daee6341cafb
Willystone <willstown@icloud.com>
2021-08-26 17:01:31 +0300

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 /*
  * File: 5-printf.c
- * Auth: Willystone K. Kalaghe
- * The printf function is in the stdio.h library
- * It reads strings and print out on the screen
+ * Auth: Willystone K Kalaghe
+ * File uses printf function to print out std output.
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is a piece of art,
- * 		followed by a new line. *
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
  * Return: Always 0.
  */
 int main(void)
