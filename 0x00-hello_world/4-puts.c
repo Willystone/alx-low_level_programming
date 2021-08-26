10575e9c934ea68ef6e991a95a78e1de20f60083
Willystone <willstown@icloud.com>
2021-08-26 16:21:45 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,17 @@
-#include <stdio.h>
-
 /*
  * This function is in standard input out put library
  * It reads & prints out charactors and strings
  * The main function returns a void
- *                         THE CODE
- * main print fuction puts out the string Programming is like a multilingual puzzle
- * followed by a new line
- * then returns a void at the end with the value 0
  */
+
+#include <stdio.h>
+
+/**
+ * main function prints out 'Programming is like a multilingual puzzle'
+ * followed by a new line.
+ * Returns: always a void
+ */
+ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
