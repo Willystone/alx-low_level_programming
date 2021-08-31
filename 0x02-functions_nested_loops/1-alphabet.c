e1d367bc2290c6e2797fde63e7a37cf48c34e26a
Willystone <willstown@icloud.com>
2021-08-31 17:27:20 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Print_alphabet - prints the alphabet in lowercasing.
+ *
+ */
+
+void print_alphabet(void)
+
+{
+	char a;
+
+	for(a = 'a'; a <= 'z'; a++)
+	{
+	_putchar(a);
+	}
+	_putchar('\n');
+}
