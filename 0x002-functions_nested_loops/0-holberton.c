f3189bf96d30f4cde08389956017b6d03fd16bab
Willystone <willstown@icloud.com>
2021-08-31 16:00:58 +0300

    holberton

---
+++ 0x002-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * File name: 0-holberton.c 
+ * Author: Willystone Kalaghe
+ *
+ * main - print function prints "Holberton"
+ * Return: always 0.
+ */
+ 
+int main()
+
+{
+	printf("Holberton\n");
+	return 0;
+
+}
