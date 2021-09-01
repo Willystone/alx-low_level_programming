209ff60816627e275bc09664b95e2a50022edf48
Willystone <willstown@icloud.com>
2021-09-01 08:53:50 +0300

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,21 @@
+#include "holberton.h"
 
-#include "main.h"
 /**
- * main - function to print prints out "Holberton"
- *
- * Return: 0
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	char holberton[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-	unsigned int c;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	for (c = 0; c < sizeof(holberton); c++)
-	{
-		_putchar(holberton[c]);
-	}
-_putchar('\n');
-return (0);
+	return (0);
 }
