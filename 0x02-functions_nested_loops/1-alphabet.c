96eeec0ed3b05c241477fa0cef62e0cf6b838e52
Willystone <willstown@icloud.com>
2021-09-01 09:19:29 +0300

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,16 @@
  * print_alphabet - Prints lowercase alphabet 10 times.
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int i = 0;
-	char abc;
+	char n;
 
 	while (i < 10)
 	{
-		for (abc = 'a'; abc <= 'z'; abc++)
+		for (n = 'a'; n <= 'z'; n++)
 		{
-			_putchar(abc);
+			_putchar(n);
 		}
 
 		_putchar('\n');
