4e6ab9d16a0b340d7ad5dcf188fa74e54ffaf611
Willystone <willstown@icloud.com>
2021-09-14 10:51:49 +0300

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,11 +6,10 @@
  * @a: Pointer for chessboard to be printed.
  */
 void print_chessboard(char (*a)[8])
-{
-	int row;
-	int col;
+{	
+	int row, col;
 
-	for (row = 0; a[row][7]; row++) 
+	for (row = 0; a[row][7]; row++)
 	{
 		for (col = 0; col < 8; col++)
 		{
@@ -18,4 +17,5 @@ void print_chessboard(char (*a)[8])
 		}
 		_putchar('\n');
 	}
+
 }
