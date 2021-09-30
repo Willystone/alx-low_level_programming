b8f1add9f4f5a3c61b66919a029492e5ce126945
Willystone <willstown@icloud.com>
2021-09-30 22:26:12 +0300

    changes made

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,7 +11,9 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i, char *str, va_list strings;
+	unsigned int i;
+	char *str;
+	va_list strings;
 
 	va_start(strings, n);
 	for (i = 0; i < n; i++)
